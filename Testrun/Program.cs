c55381eb2f4181c15c01bf44a2e37d0e664c2b98
Ebube <onyirimbaebubechi1234@gmail.com>
2024-02-16 16:18:22 +0100

    Discount_sales

---
+++ Testrun/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testrun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int denominator = 5;
+            int numerator = ++denominator;
+            Console.WriteLine(numerator);
+        }   
+    }
+}
